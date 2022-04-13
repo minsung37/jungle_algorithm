dd38eca7a355161b7f816b25ff4f6667e3d07a06
minsung37 <tampang99@naver.com>
2022-04-13 19:59:02 +0900

    뱀

---
+++ 2-4. 큐/23. 뱀.py
@@ -0,0 +1,69 @@
+# 맵
+n = int(input())
+board = [[0] * (n + 1) for _ in range(n + 1)]
+
+# 사과있는곳은 1로 표시
+k = int(input())
+for _ in range(k):
+    x, y = map(int, input().split())
+    board[x][y] = 1
+
+# 방향 회전 정보
+l = int(input())
+info = []
+for _ in range(l):
+    x, c = input().split()
+    info.append([int(x), c])
+
+# 처음에는 오른쪽을 보고 있으므로(동, 남, 서, 북)
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+def turn(direction, c):
+    # 왼쪽으로 회전
+    if c == "L":
+        direction = (direction - 1) % 4
+    # 오른쪽으로 회전
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+
+def simulate():
+    x, y = 1, 1  # 뱀의 머리 위치
+    board[x][y] = 2  # 뱀
+    direction = 0  # 처음에는 동쪽을 보고 있음
+    time = 0  # 시작한 뒤에 지난 '초' 시간
+    index = 0  # 다음에 회전할 정보
+    q = [(x, y)]  # 뱀이 차지하고 있는 위치정보(꼬리가 앞쪽)
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        # 맵 범위 안에 있고, 뱀의 몸통이 없는 위치라면
+        if 1 <= nx and nx <= n and 1 <= ny and ny <= n and board[nx][ny] != 2:
+            # 사과가 없다면 이동 후 꼬리 제거
+            if board[nx][ny] == 0:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                board[px][py] = 0
+            # 사과가 있다면 이동 후에 꼬리 그대로 두기
+            if board[nx][ny] == 1:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+        # 벽이나 뱀의 몸통과 부딪혔다면
+        else:
+            time = time + 1
+            break
+        # 다음 위치로 머리를 이동
+        x, y = nx, ny
+        time = time + 1
+        # 회전할 시간인 경우 회전
+        if index < l and time == info[index][0]:
+            direction = turn(direction, info[index][1])
+            index = index + 1
+    return time
+
+
+print(simulate())
