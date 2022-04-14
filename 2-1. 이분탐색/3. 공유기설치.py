75e1b2788ae9e10c62ba22a17acd5d43f12537da
minsung37 <tampang99@naver.com>
2022-04-14 14:36:18 +0900

    4/14

--- 2-1. 이분탐색/3. 공유기설치.py
+++ 2-1. 이분탐색/3. 공유기설치.py
@@ -1,35 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-# n개의 집에 c개의 공유기 설치
 n, c = map(int, input().split())
-array = [int(input()) for _ in range(n)]
-array.sort()
+array = sorted([int(input()) for _ in range(n)])
 
-# 공유기 간격의 최소값, 최대값
-mn = 1
-mx = array[-1] - array[0]
+start = -1
+end = 10**9+1
 
-# 인접한공유기 사이의 최대 거리
-result = 0
-
-# 이분탐색
-while mn <= mx:
-    mid = (mx + mn) // 2
-    # 초기 공유기 설치 위치
+while start < end:
+    mid = (end + start) // 2
     spot = array[0]
     count = 1
     for i in range(1, n):
-        # 공유기 설치하기
         if array[i] >= spot + mid:
             spot = array[i]
             count = count + 1
-    # 더 설치 해야함 => 간격 좁히기
-    if count < c:
-        mx = mid - 1
-    # 목표개수 오바한경우 => 간격 줄여야함
+    if c <= count:
+        start = mid + 1
     else:
-        mn = mid + 1
-        result = mid
+        end = mid
 # 정답출력
-print(result)
+print(start - 1)
