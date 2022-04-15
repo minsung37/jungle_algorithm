9339336e71d0f9de12b45af8c8d5a68ad15cd1e7
minsung37 <tampang99@naver.com>
2022-04-15 14:28:05 +0900

    4/15

--- 3-1. 그래프 탐색 기본/크루스칼/1. 기본적인 서로소 집합 알고리즘.py
+++ 3-1. 그래프 탐색 기본/크루스칼/1. 기본적인 서로소 집합 알고리즘.py
@@ -31,7 +31,7 @@ for i in range(e):
     union_parent(parent, a, b)
 
 # 각 원소가 속한 집합 출력
-print('각 원소가 속합 집합: ', end='')
+print('각 원소가 속한 집합: ', end='')
 for i in range(1, v + 1):
     print(find_parent(parent, i), end=' ')
 print()
