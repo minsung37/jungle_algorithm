bfff5c4769d6fdbabb96895b4a8eef4ead3d50c2
minsung37 <tampang99@naver.com>
2022-04-26 00:16:57 +0900

    멀티탭

--- 4-2. 그리디/14. 멀티탭스케줄링.py
+++ 4-2. 그리디/14. 멀티탭스케줄링.py
@@ -2,33 +2,34 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-array = list(map(int, input().split()))
-plug, count = [], 0
+plug = list(map(int, input().split()))
+tap, count = [], 0
 
 for i in range(k):
-    # 플러그에 없을때 꽂기
-    if array[i] not in plug:
-        # 꽉 찬경우 빼고 꽂기
-        if len(plug) == n:
-            for j in plug:
-                # j가 꽂을 번호에 없을때
-                if j not in array[i:k]:
-                    plug.remove(j)
-                    plug.append(array[i])
+    # 멀티탭에 없을때 꽂기
+    if plug[i] not in tap:
+        # 꽉 찬경우 플러그 빼고 꽂기
+        if len(tap) == n:
+            for j in tap:
+                # j가 더이상 안쓰일때 뽑는다
+                if j not in plug[i:k]:
+                    tap.remove(j)
+                    tap.append(plug[i])
                     count = count + 1
                     break
             # 둘다 있는 경우 => 나중에 오는걸 빼야함
             else:
-                # 뽑으면 안되는 애들 순서대로 담기
-                plug_temp = []
-                for x in array[i:k]:
-                    for y in plug:
-                        if x == y and x not in plug_temp:
-                            plug_temp.append(x)
-                plug.remove(plug_temp[-1])
-                plug.append(array[i])
+                # 우선순위 순으로 스택에 담기
+                tap_temp = []
+                for x in plug[i:k]:
+                    for y in tap:
+                        if x == y and x not in tap_temp:
+                            tap_temp.append(x)
+                # 우선순위 마지막을 탭에서 뽑기
+                tap.remove(tap_temp[-1])
+                tap.append(plug[i])
                 count = count + 1
         # 플러그에 꽂기
         else:
-            plug.append(array[i])
+            tap.append(plug[i])
 print(count)
